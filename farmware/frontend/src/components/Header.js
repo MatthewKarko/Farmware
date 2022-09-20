6cb19f0e5a2d150f34ac424040d74ecb69e5728c
mkar6206 <mkar6206@uni.sydney.edu.au>
2022-09-20 22:10:52 +1000

    created responsive navigation bar

--- farmware/frontend/src/components/Header.js
+++ farmware/frontend/src/components/Header.js
@@ -1,25 +1,65 @@
 import React from 'react';
-import { AppBar } from '@mui/material';
+import { BrowserRouter as Router, Route, Redirect, Switch, Link } from 'react-router-dom';
+import { AppBar, Tabs, Tab, Button, useMediaQuery, useTheme, IconButton } from '@mui/material';
 import { Toolbar } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import CssBaseline from '@mui/material/CssBaseline';
 import { makeStyles } from '@mui/material';
+import LoginPage from '../webpages/LoginPage';
+import DrawerComp from './DrawerComp';
+import AgricultureIcon from '@mui/icons-material/Agriculture';
 
+const PAGES = ["Login", "Signup"];
 
 const Header = () => {
+    const theme = useTheme();
+    const isMatch = useMediaQuery(theme.breakpoints.down('md'));
+
+
 
     return(
         <React.Fragment>
             <AppBar
                 position="static"
-                
+                sx={{background: "#026946"}}
             >
                 <Toolbar>
-                    <Typography variant="h6" color="inherit" noWrap>
-                        Farmware
-                    </Typography>
-                </Toolbar>
+                <IconButton href="/" sx={{color: 'white'}}>
+                                    <AgricultureIcon
+                                    sx={{marginRight: '15px'}}
+                                    />
+                </IconButton>
+                <Typography variant="h6" color="inherit" noWrap  sx={{flexGrow: 1}}>
+                            Farmware
+                </Typography>
+                    
+                    {
+                        isMatch ? (
+                            <>
+                        
+                            <DrawerComp />
+                            </>
+                        ) : (
+                            <>
+                                
+                               
+                            
+                            
+                            {
+                                PAGES.map((page) => (
+                            
+                                    <Button sx={{marginLeft: 'auto'}} variant='contained' href={`/${page.toLowerCase()}`}>  
+                                    {page}
+                                    </Button>
+                                ))   
+                            }
+                            </>
+                        )
+                    }
 
+                    
+                </Toolbar>
+        
             </AppBar>
         </React.Fragment>
     );
--- farmware/frontend/src/index.js
+++ farmware/frontend/src/index.js
@@ -4,11 +4,15 @@ import App from "./App";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LoginPage from "./webpages/LoginPage";
+import SignUpPage from "./webpages/SignUpPage";
 const routing = (
     <Router>
         <Header />
         <Routes>
             <Route path="/" element={<App />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />}/>
         </Routes>
         <Footer />
     </Router>
