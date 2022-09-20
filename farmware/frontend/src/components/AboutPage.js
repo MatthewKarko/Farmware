2d49b98bb19c837b465709c8fd20bcfd23d29eee
Utkarsh <uvas7225@uni.sydney.edu.au>
2022-09-20 20:24:29 +1000

    completed the about page in react

---
+++ farmware/frontend/src/components/AboutPage.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+export default class AboutUsPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+          <h1 style="margin-left: 650px;font-family: verdana;" >About Us</h1>
+          <p style="font-family: verdana;">The current farming industry has raised interest regarding redundancy in their inventory management systems and their data presentation when they incur regulatory visits, and they would like for there to be a system in place to simplify this process.The farming industry is upheld to tight regulatory standards. Freshcare provides internationally recognised assurance standards for the fresh produce industries. Freshcare helps Australian producers demonstrate their capacity to grow, pack, and distribute safe, sustainable, quality products for customers and consumers worldwide. Currently, a digital farm management tool to help facilitate the regulatory requirements does not exist—analogue paper trails are common practice—leading to significant losses in productivity.</p>
+  <p><b style="font-family: Lucida;">
+          This is where we come into the picture.
+  </b></p>
+     <p style="font-family: verdana;">
+          Farmware services the need for a digitised regulatory compliance aid—it provides an interface for farm admins and workers to collect and manage the required data for Freshcare standards. Farmware can further leverage this data to power business intelligence and provide deep insight into the farm’s practices to assist decisions.
+          </p>
+
+        );
+    }
+}
