13b5f3a84be4186d0f65d477d34790cbe1f6f91a
Utkarsh <uvas7225@uni.sydney.edu.au>
2022-09-20 20:31:33 +1000

    updated homepage.js

--- farmware/frontend/src/components/HomePage.js
+++ farmware/frontend/src/components/HomePage.js
@@ -2,19 +2,20 @@ import React, {Component} from 'react';
 
 import LoginPage from "./LoginPage";
 import SignUpPage from "./SignUpPage";
+import AboutPage from "./AboutPage";
 
-import { 
-    BrowserRouter as Router, 
-    Routes, 
-    Route, 
-    Link, 
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
     Redirect
 } from "react-router-dom";
 
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
-    } 
+    }
 
     render() {
         // return <p>hello</p>;
@@ -22,6 +23,7 @@ export default class HomePage extends Component {
         <Routes>
             <Route path='/' element={<p>hi</p>}/>
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/about" element={<AboutPage />} />
         </Routes>
     </Router>);
     }
