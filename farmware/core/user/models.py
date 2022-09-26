fd9cac73ab2cac875d228d5bce4a980a4739db76
sand5508 <sand5508@uni.sydney.edu.au>
2022-09-26 18:02:57 +1000

    fixed issue with circular dependency

--- farmware/core/user/models.py
+++ farmware/core/user/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
-from ..api.models import Organisation, Team
-
 class UserManager(BaseUserManager):
     """Define a model manager for User model with no username field."""
 
@@ -52,7 +50,7 @@ class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
 
     organisation = models.ForeignKey(
-        Organisation,
+        'core_api.Organisation',
         on_delete=models.CASCADE
     )
 
@@ -64,7 +62,7 @@ class User(AbstractUser):
         )
 
     teams = models.ManyToManyField(
-        Team,
+        'core_api.Team',
         blank=True
     )
 
@@ -88,73 +86,3 @@ class Admin(User):
         if not self.pk: self.role = User.Roles.ADMIN
 
         return super().save(*args, **kwargs)
-
-class Organisation(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    logo = models.CharField(max_length=50)
-    owner_id = models.ForeignKey(to=User, on_delete=models.DO_NOTHING)
-
-class OrganisationCode(models.Model):
-    org_code = models.CharField(primary_key=True, max_length=16)
-    org_id = models.ForeignKey(Organisation, on_delete=models.DO_NOTHING)
-
-class Produce(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextField(max_length=100)
-
-class ProduceVariety(models.Model):
-    id = models.AutoField(primary_key=True)
-    produce_id = models.ForeignKey(Produce, on_delete=models.DO_NOTHING)
-    variety = models.TextField(max_length=100)
-
-class ProduceQuantitySuffix(models.Model):
-    id = models.AutoField(primary_key=True)
-    produce_id = models.ForeignKey(Produce, on_delete=models.DO_NOTHING)
-    suffix = models.TextField(max_length=20)
-    base_equivalent = models.FloatField()
-
-class Supplier(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextField(max_length=100)
-    phone_number = models.TextField(max_length=10)
-
-class AreaCode(models.Model):
-    area_code = models.TextField(primary_key=True, max_length=50)
-    description = models.TextField(max_length=200)
-
-class Stock(models.Model):
-    id = models.AutoField(primary_key=True)
-    produce_id = models.ForeignKey(Produce, on_delete=models.DO_NOTHING)
-    variety_id = models.ForeignKey(ProduceVariety, on_delete=models.DO_NOTHING)
-    quantity = models.FloatField()
-    quantity_suffix_id = models.ForeignKey(ProduceQuantitySuffix, on_delete=models.DO_NOTHING)
-    supplier_id = models.ForeignKey(Supplier, on_delete=models.DO_NOTHING)
-    date_seeded = models.DateTimeField()
-    date_planted = models.DateTimeField()
-    date_picked = models.DateTimeField()
-    ehd = models.DateTimeField() # earliest harvest date
-    date_completed = models.DateTimeField()
-    area_code = models.ForeignKey(AreaCode, on_delete=models.DO_NOTHING)
-
-class StockPickers(models.Model):
-    id = models.AutoField(primary_key=True)
-    stock_id = models.ForeignKey(Stock, on_delete=models.DO_NOTHING)
-    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-
-class Customer(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextField(max_length=50)
-    phone_number = models.TextField(max_length=10)
-
-class Order(models.Model):
-    id = models.AutoField(primary_key=True)
-    customer_id = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
-
-class OrderStock(models.Model):
-    id = models.AutoField(primary_key=True)
-    order_id = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
-    stock_id = models.ForeignKey(Stock, on_delete=models.DO_NOTHING)
-    quantity = models.FloatField()
-    quantity_suffix_id = models.ForeignKey(ProduceQuantitySuffix, on_delete=models.DO_NOTHING)
-    invoice_number = models.TextField(max_length=20)
