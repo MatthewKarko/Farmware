d20f8e12d93605d0ac8c7acd18e5157a4e4cba7d
Utkarsh <uvas7225@uni.sydney.edu.au>
2022-10-09 02:09:42 +1100

    added more tests

--- farmware/core/api/tests.py
+++ farmware/core/api/tests.py
@@ -14,9 +14,15 @@ class OrganisationTestCases(TestCase):
         self.assertEqual(farmone.logo, "goat")
         self.assertEqual(farmone.name, "Farmtwo")
         self.assertEqual(farmone.logo, "sheep")
+        self.assertRaises(Organisaton.objects.create(name=4,logo="sheep"))
+        self.assertRaises(Organisaton.objects.create(name="anothername",logo=5.0))
+        self.assertRaises(Organisaton.objects.create(name="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo cursus condimentum. Donec pulvinar odio sed enim tristique, sit amet tristique dolor volutpat. Proin nec mauris gravida libero scelerisque consectetur non at sapien. Nam et felis nibh. Morbi eget augue sit amet nisl elementum congue. Nulla vel laoreet velit. Nullam est neque, efficitur sodales suscipit ac, vulputate eget velit. Donec.",logo="i"))
+        self.assertRaises(Organisaton.objects.create(name="a",logo="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo cursus condimentum. Donec pulvinar odio sed enim tristique, sit amet tristique dolor volutpat. Proin nec mauris gravida libero scelerisque consectetur non at sapien. Nam et felis nibh. Morbi eget augue sit amet nisl elementum congue. Nulla vel laoreet velit. Nullam est neque, efficitur sodales suscipit ac, vulputate eget velit. Donec."))
+
 class   UserTestCases(TestCase):
     def setUp(self):
         User.objects.create(email="example@gmail.com",first_name="first_name",last_name="last_name",organisation="organisation",password="password")
+
     def test_User(self):
         user = User.objects.get(name="first_name")
         self.assertEqual(user.email, "example@gmail.com")
@@ -25,22 +31,32 @@ class   UserTestCases(TestCase):
         self.assertEqual(user.password,"password")
 class   AreaCodeTestCases(TestCase):
     def setUp(self):
-        AreaCode.objects.create( area_code=204,description="just another area code")
+        AreaCode.objects.create( area_code="204",description="just another area code")
     def test_AreaCode(self):
-        areacode = AreaCode.objects.get(area_code=204)
+        areacode = AreaCode.objects.get(area_code="204")
         self.assertEqual(areacode.description,"just another area code")
+        self.assertRaises(AreaCode.objects.create( area_code="204",description="just another area code"))
+        self.assertRaises(AreaCode.objects.create( area_code="1234567890123456789013456789012345678901234567890123456789",description="ss"))
+        self.assertRaises(AreaCode.objects.create( area_code="203", description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque egestas, leo in mattis suscipit, ante arcu gravida sapien, sit amet varius quam mi vitae lectus. Nullam in aliquam odio. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In quis imperdiet purus. Curabitur quis laoreet tellus. Quisque gravida vitae arcu molestie interdum. Phasellus dictum urna est, ut feugiat turpis gravida sed. Pellentesque ut consectetur ante. Sed scelerisque mauris quis mi condimentum, a consectetur massa ullamcorper. Ut condimentum tellus ac lorem luctus ultrices. In nibh est, placerat ac dignissim sit amet, dapibus in libero. Quisque lobortis lacus et laoreet hendrerit. Quisque vel placerat mi. Suspendisse vitae sodales justo, sed dapibus sapien. Vestibulum bibendum fermentum fringilla. Aliquam vitae neque orci.Aenean euismod lacus id orci rutrum suscipit. In sit amet tortor vel mauris luctus lacinia. Suspendisse potenti. Mauris lorem sem, ornare in justo commodo, vulputate condimentum urna. Nam fermentum ipsum vestibulum vehicula cursus. Mauris iaculis ut risus a imperdiet. Suspendisse porttitor, sem sed laoreet luctus, est tellus bibendum felis, a dignissim justo velit a ante. Sed placerat justo eros, at viverra ex imperdiet ac. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec id odio nunc. Nulla tincidunt rhoncus tellus, sit amet maximus lectus blandit sit amet. Donec interdum vitae sapien sit amet varius. Proin imperdiet mauris eget cursus rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent id libero erat.Pellentesque gravida, lectus vitae porttitor dapibus, nisl nunc suscipit ante, eu varius sem dolor id ante. Pellentesque porta feugiat ipsum, eget pharetra dolor dapibus vitae. Donec gravida eleifend sem, ac dictum augue elementum eget. Ut accumsan maximus purus, sed aliquet tellus interdum vel. Praesent iaculis diam et neque rhoncus placerat. Mauris consectetur vel leo nec suscipit. Praesent placerat, felis facilisis."))
 class  CustomerTestCases(TestCase):
     def setUp(self):
         Customer.objects.create(name = "Henry",phone_number="9191223445" )
     def test_Customer(self):
         customer = Customer.objects.get(name = "Henry")
         self.assertEqual(customer.phone_number,"9191223445")
+        self.assertRaises(Customer.objects.create(name = "Henr",phone_number="91912234452" ))
+        self.assertRaises(Customer.objects.create(name = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas commodo cursus condimentum. Donec pulvinar odio sed enim tristique, sit amet tristique dolor volutpat. Proin nec mauris gravida libero scelerisque consectetur non at sapien. Nam et felis nibh. Morbi eget augue sit amet nisl elementum congue. Nulla vel laoreet velit. Nullam est neque, efficitur sodales suscipit ac, vulputate eget velit. Donec.",phone_number="9191223445")
 class  SupplierTestCases(TestCase):
     def setUp(self):
         Supplier.objects.create(name = "john",phone_number = "1234567891")
     def test_Supplier(self):
         supplier = Supplier.objects.get(name = "Henry")
         self.assertEqual(supplier.phone_number,"1234567891")
+        self.assertRaises(Supplier.objects.create(name = "john",phone_number = "12345678911"))
+        self.assertRaises(Supplier.objects.create(name ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et justo ut turpis suscipit mattis ac ac lorem. Aenean molestie nisi et ullamcorper condimentum. Integer at congue nulla, quis elementum mauris. Mauris luctus nisl elementum massa vehicula, ut maximus purus pellentesque. Curabitur consectetur tincidunt malesuada. Sed dignissim ipsum nec urna tincidunt lobortis. Vestibulum porta finibus tincidunt. Nunc a odio porta, aliquam odio ut, commodo risus. Duis quis risus in nulla accumsan tempus dapibus et elit. Integer sed est at ligula commodo tristique.Sed et pulvinar mauris. Vivamus fringilla odio a ex porttitor, nec dapibus nisi vestibulum. Phasellus sed nisi velit. Vestibulum tempus justo dolor, et pellentesque urna molestie ut. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur viverra tincidunt hendrerit. Maecenas accumsan est vitae est faucibus facilisis. Sed nec quam in orci vehicula varius in ut urna. Proin lacus nibh, suscipit a elementum nec.",phone_number = "1234567891"))
+        self.assertRaises(Supplier.objects.create(name = 2,phone_number = "1234567891"))
+        self.assertRaises(Supplier.objects.create(name = "name2",phone_number = 1234567891))
+
 class ProduceVarietyTestCases(TestCase):
     def setUp(self):
         produce=Produce.objects.create(name="eggs")
@@ -48,6 +64,8 @@ class ProduceVarietyTestCases(TestCase):
     def test_ProduceVariety(self):
         producevariety=ProduceVariety.objects.get(variety="brown")
         self.assertEqual(producevariety.variety,"brown")
+        self.assertRaises(ProduceVariety.objects.create(produce_id=produce,variety="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et justo ut turpis suscipit mattis ac ac lorem. Aenean molestie nisi et ullamcorper condimentum. Integer at congue nulla, quis elementum mauris. Mauris luctus nisl elementum massa vehicula, ut maximus purus pellentesque. Curabitur consectetur tincidunt malesuada. Sed dignissim ipsum nec urna tincidunt lobortis. Vestibulum porta finibus tincidunt. Nunc a odio porta, aliquam odio ut, commodo risus. Duis quis risus in nulla accumsan tempus dapibus et elit. Integer sed est at ligula commodo tristique.Sed et pulvinar mauris. Vivamus fringilla odio a ex porttitor, nec dapibus nisi vestibulum. Phasellus sed nisi velit. Vestibulum tempus justo dolor, et pellentesque urna molestie ut. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur viverra tincidunt hendrerit. Maecenas accumsan est vitae est faucibus facilisis. Sed nec quam in orci vehicula varius in ut urna. Proin lacus nibh, suscipit a elementum nec"))
+
 class ProduceQuantitySuffixTestCases(TestCase):
     def setUp(self):
         produce=Produce.objects.create(name="eggs")
@@ -55,6 +73,7 @@ class ProduceQuantitySuffixTestCases(TestCase):
     def test_ProduceQuantitySuffix(self):
         producequantitysuffix=ProduceQuantitySuffix.objects.get(suffix="lorem ipsum")
         self.assertEqual(producequantitysuffix.base_equivalent,5.0)
+        self.assertRaises(ProduceQuantitySuffix.objects.create(produce_id=produce,suffix="ipsumLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur cursus lectus id est dignissim dapibus. Integer mollis sodales urna, quis consectetur enim aliquam nec. Fusce nec velit nec lacus sollicitudin bibendum a ut augue. Fusce commodo lacus vel enim vulputate finibus. Integer aliquam quam at lorem imperdiet dignissim. Suspendisse volutpat.",base_equivalent=5.0))
 class StockTestCases(TestCase):
     def setUp(self):
         produce=Produce.objects.create(name="eggs")
@@ -108,6 +127,10 @@ class TeamTestCases(TestCase):
     def test_Team(self):
         team=Team.objects.get(category ="j")
         self.assertEqual(team.name,"jack")
+        self.assertRaises(Team.objects.create(category ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi convallis euismod gravida. Vestibulum quam lacus, faucibus ac dui nec, hendrerit lobortis arcu. Ut vel lorem at enim dignissim porttitor in nec neque. Mauris lobortis justo lorem, id venenatis dui laoreet vel. Quisque egestas neque quis erat porttitor fermentum. Proin cursus, lorem non auctor aliquam, turpis neque tincidunt sem, vitae maximus massa dolor ut diam. Nam euismod urna sed leo vestibulum ultrices. Donec tempus fringilla feugiat. Nullam faucibus mattis diam, in sagittis lacus aliquam eget. Mauris eu ligula fermentum, bibendum enim id, sodales turpis. Praesent sed risus felis. Vivamus ut ultrices nisl.Ut luctus purus neque, eget blandit augue consectetur at. Etiam eleifend cursus tortor, ut venenatis lectus laoreet sollicitudin. Nunc at elementum magna. Integer ut scelerisque arcu, ac pellentesque lorem. Donec rutrum porttitor consectetur. Nunc nunc enim, sollicitudin mollis maximus non, lacinia at neque. Curabitur ultrices tincidunt pharetra. Vestibulum vitae.",name = "jack",organisation = organisation))
+
+        self.assertRaises(Team.objects.create(category ="Jack",name = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi convallis euismod gravida. Vestibulum quam lacus, faucibus ac dui nec, hendrerit lobortis arcu. Ut vel lorem at enim dignissim porttitor in nec neque. Mauris lobortis justo lorem, id venenatis dui laoreet vel. Quisque egestas neque quis erat porttitor fermentum. Proin cursus, lorem non auctor aliquam, turpis neque tincidunt sem, vitae maximus massa dolor ut diam. Nam euismod urna sed leo vestibulum ultrices. Donec tempus fringilla feugiat. Nullam faucibus mattis diam, in sagittis lacus aliquam eget. Mauris eu ligula fermentum, bibendum enim id, sodales turpis. Praesent sed risus felis. Vivamus ut ultrices nisl.Ut luctus purus neque, eget blandit augue consectetur at. Etiam eleifend cursus tortor, ut venenatis lectus laoreet sollicitudin. Nunc at elementum magna. Integer ut scelerisque arcu, ac pellentesque lorem. Donec rutrum porttitor consectetur. Nunc nunc enim, sollicitudin mollis maximus non, lacinia at neque. Curabitur ultrices tincidunt pharetra. Vestibulum vitae.",organisation = organisation))
+        
 ###class StockPickersTestCases(TestCase):
     #def setUp(self):
     #    produce=Produce.objects.create(name="eggs")
