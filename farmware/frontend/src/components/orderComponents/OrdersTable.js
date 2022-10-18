fd32a45b817ef52823f4657ad4e8546496efcd02
lhen4176 <lhen4176@uni.sydney.edu.au>
2022-10-18 13:56:22 +1100

    view order progress

--- farmware/frontend/src/components/orderComponents/OrdersTable.js
+++ farmware/frontend/src/components/orderComponents/OrdersTable.js
@@ -1,47 +1,88 @@
 import React, { useState, useEffect, Fragment } from "react";
 import { useNavigate } from 'react-router-dom';
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Typography } from "@mui/material"
+import { Grid, Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Typography } from "@mui/material"
 import '../../css/PageMargin.css';
 import '../../css/Modal.css';
 import ordersData from "./mock-orders.json";
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import PendingActionsIcon from '@mui/icons-material/PendingActions';
 
 function OrdersTable() {
   const navigate = useNavigate();
 
-    function handleViewOrderClick(order) {
-        navigate("/view-order",{state:order});
-      }
+  function handleViewOrderClick(order) {
+    navigate("/view-order", { state: order });
+  }
 
   return (
     <>
-        <div className="main-content">
-                <Typography variant="h4" sx={{
-                    fontFamily: 'Lato',
-                    fontWeight: 'bold',
-                    paddingBottom: '20px',
-                }}>Orders Table</Typography>
+      <div className="main-content">
 
-        <TableContainer component={Paper} className="table" style={{ maxWidth: 800, margin:"auto"}}>
-          <Table aria-label="simple table" style={{ maxWidth: 800, margin:"auto"}}>
+        <Box sx={{ width: '100%', height: '10%' }}>
+          <Grid container rowSpacing={0} columnSpacing={{ xs: 6, sm: 2, md: 4 }}
+            style={{ minHeight: '10vh' }}>
+
+            <Grid item xs={6}>
+              <Typography variant="h4" sx={{
+                fontFamily: 'Lato',
+                fontWeight: 'bold',
+                paddingBottom: '20px',
+              }}>Orders Table</Typography>
+            </Grid>
+
+            <Grid item xs={6} sx={{ textAlign: "right" }}>
+              <Button type="submit" variant="outlined" size="large" style={{
+                color: "#028357",
+                borderColor: "#028357",
+                marginRight: "10px"
+              }}
+                onClick={() => { }}
+              >Create Order</Button>
+            </Grid>
+          </Grid>
+        </Box>
+
+        <TableContainer component={Paper} className="table" style={{ margin: "auto" }}>
+          <Table aria-label="simple table" style={{ margin: "auto" }}>
+            <colgroup>
+              <col style={{ width: '8%' }} />
+              <col style={{ width: '12%' }} />
+              <col style={{ width: '16%' }} />
+              <col style={{ width: '20%' }} />
+              <col style={{ width: '20%' }} />
+              <col style={{ width: '10%' }} />
+              <col style={{ width: '14%' }} />
+            </colgroup>
             <TableHead>
               <TableRow>
-                <TableCell className="tableCell">Order ID</TableCell>
-                <TableCell className="tableCell">Customer</TableCell>
-                <TableCell className="tableCell">Invoice Number</TableCell>
-                <TableCell className="tableCell">Date Created</TableCell>
-                <TableCell className="tableCell">Status</TableCell>
-                <TableCell className="tableCell"></TableCell>
+                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Order ID</TableCell>
+                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Customer ID</TableCell>
+                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Invoice Number</TableCell>
+                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Date Created</TableCell>
+                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Completion Date</TableCell>
+                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Status</TableCell>
+                <TableCell className="tableCell" sx={{ textAlign: "center" }}></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {ordersData.map((order) => (
-                <TableRow key={order.order_id}>
-                  <TableCell className="tableCell">{order.order_id}</TableCell>
-                  <TableCell className="tableCell">{order.customer}</TableCell>
-                  <TableCell className="tableCell">{order.invoice_number}</TableCell>
-                  <TableCell className="tableCell">{order.date}</TableCell>
-                  <TableCell className="tableCell">{order.status}</TableCell>
-                  <TableCell className="tableCell">
+                <TableRow key={order.order_id} >
+                  <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order.id}</TableCell>
+                  <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order.customer_id}</TableCell>
+                  <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order.invoice_number}</TableCell>
+                  <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order.order_date}</TableCell>
+                  <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order.completion_date}</TableCell>
+                  {order.completion_date == "" &&
+                    <TableCell className="tableCell" sx={{ textAlign: "center" }}>
+                      <PendingActionsIcon />
+                    </TableCell>
+                  }
+                  {order.completion_date != "" &&
+                    <TableCell className="tableCell" sx={{ textAlign: "center" }}>
+                      <CheckBoxIcon sx={{ color: "#028357" }} />
+                    </TableCell>
+                  }
+                  <TableCell className="tableCell" sx={{ textAlign: "center" }}>
                     <Button variant="outlined" size="medium" onClick={() => handleViewOrderClick(order)}
                     >View Order</Button>
                   </TableCell>
--- farmware/frontend/src/components/orderComponents/ViewOrder.js
+++ farmware/frontend/src/components/orderComponents/ViewOrder.js
@@ -1,59 +1,93 @@
 import React, { useState, useEffect, Fragment } from "react";
-import { useNavigate } from 'react-router-dom';
-import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Typography } from "@mui/material"
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Grid, Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Typography } from "@mui/material"
 import '../../css/PageMargin.css';
 import '../../css/Modal.css';
-import ordersData from "./mock-orders.json";
+import orderData from "./mock-order-items.json";
 
 function OrdersTable() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
+    const location = useLocation();
 
-    // function handleViewOrderClick(order) {
-    //     navigate("/orders/view-order",{state:order});
-    //   }
+    function handleViewProduceClick(order_item) {
+        alert("View produce clicked: " + order_item.stock_id);
+        // navigate("/orders/view-order",{state:order});
+    }
 
-  return (
-    <>
-        <div className="main-content">
-                <Typography variant="h4" sx={{
-                    fontFamily: 'Lato',
-                    fontWeight: 'bold',
-                    paddingBottom: '20px',
-                }}>Customer Name</Typography>
+    function markAsComplete() {
+        alert("Mark order as complete. Order id: " + location.state.id);
+        // navigate("/orders/view-order",{state:order});
+    }
+    return (
+        <>
+            <div className="main-content">
+                <Box sx={{ width: '100%', height: '10%' }}>
+                    <Grid container rowSpacing={0} columnSpacing={{ xs: 6, sm: 2, md: 4 }}
+                        style={{ minHeight: '10vh' }}>
 
-        {/* <TableContainer component={Paper} className="table" style={{ maxWidth: 800, margin:"auto"}}>
-          <Table aria-label="simple table" style={{ maxWidth: 800, margin:"auto"}}>
-            <TableHead>
-              <TableRow>
-                <TableCell className="tableCell">Order ID</TableCell>
-                <TableCell className="tableCell">Customer</TableCell>
-                <TableCell className="tableCell">Invoice Number</TableCell>
-                <TableCell className="tableCell">Date Created</TableCell>
-                <TableCell className="tableCell">Status</TableCell>
-                <TableCell className="tableCell"></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {ordersData.map((order) => (
-                <TableRow key={order.order_id}>
-                  <TableCell className="tableCell">{order.order_id}</TableCell>
-                  <TableCell className="tableCell">{order.customer}</TableCell>
-                  <TableCell className="tableCell">{order.invoice_number}</TableCell>
-                  <TableCell className="tableCell">{order.date}</TableCell>
-                  <TableCell className="tableCell">{order.status}</TableCell>
-                  <TableCell className="tableCell">
-                    <Button variant="outlined" size="medium" onClick={() => handleViewOrderClick(order)}
-                    >View Order</Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer> */}
+                        <Grid item xs={6}>
+                            <Typography variant="h4" sx={{
+                                fontFamily: 'Lato',
+                                fontWeight: 'bold',
+                                paddingBottom: '20px',
+                            }}>Customer ID: {location.state.customer_id}, Order ID: {location.state.id}</Typography>
 
-      </div>
-    </>
-  )
+                        </Grid>
+
+                        <Grid item xs={6} sx={{ textAlign: "right" }}>
+                            <Button type="submit" variant="outlined" size="large" style={{
+                                color: "#028357",
+                                borderColor: "#028357",
+                                marginRight: "10px"
+                            }}
+                                onClick={() => { markAsComplete() }}
+                            >Mark As Complete</Button>
+                        </Grid>
+                    </Grid>
+                </Box>
+
+                <TableContainer component={Paper} className="table" style={{ margin: "auto" }}>
+                    <Table aria-label="simple table" style={{ margin: "auto" }}>
+                        <colgroup>
+                            <col style={{ width: '17%' }} />
+                            <col style={{ width: '17%' }} />
+                            <col style={{ width: '17%' }} />
+                            <col style={{ width: '17%' }} />
+                            <col style={{ width: '17%' }} />
+                            <col style={{ width: '15%' }} />
+                        </colgroup>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Stock ID</TableCell>
+                                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Item</TableCell>
+                                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Quantity</TableCell>
+                                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Suffix</TableCell>
+                                <TableCell className="tableCell" sx={{ textAlign: "center" }}>Supplier</TableCell>
+                                <TableCell className="tableCell" sx={{ textAlign: "center" }}></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {orderData.map((order_item) => (
+                                <TableRow key={order_item.stock_id} >
+                                    <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order_item.stock_id}</TableCell>
+                                    <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order_item.item_name}</TableCell>
+                                    <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order_item.qty}</TableCell>
+                                    <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order_item.SUFFIX}</TableCell>
+                                    <TableCell className="tableCell" sx={{ textAlign: "center" }}>{order_item.supplier_id}</TableCell>
+                                    <TableCell className="tableCell" sx={{ textAlign: "center" }}>
+                                        <Button variant="outlined" size="medium" onClick={() => handleViewProduceClick(order_item)}
+                                        >View Produce</Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
+
+            </div>
+        </>
+    )
 }
 
 export default OrdersTable
