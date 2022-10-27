70be6b7b00e8aa77c1baf5cd9c1ead8af50c9ac3
Keegan <kpot0443@uni.sydney.edu.au>
2022-10-27 17:48:28 +1100

    refactor custom fields

--- farmware/core/api/serialisers/stock.py
+++ farmware/core/api/serialisers/stock.py
@@ -2,40 +2,8 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from rest_framework import serializers
 
+from .fields.stock import StockListField
 from ..models.stock import Stock, StockPickers
-
-# TODO: move
-class AddStockField(serializers.Field):
-
-    default_error_messages = {
-    'not_provided': "'{context}' is not provided.",
-    'wrong_type': "'{context}' is not of type {type}."
-}
-
-    def to_representation(self, value):
-        return {
-            'stock_id': value.get('stock_id'),
-            'quantity': value.get('quantity')
-        }
-
-    def to_internal_value(self, data: dict):
-        if data.get('stock_id', False) is False: 
-            self.fail('not_provided', context='stock_id')
-        if data.get('quantity', False) is False: 
-            self.fail('not_provided', context='quantity')
-
-        try:
-            float(data.get('quantity'))  # type: ignore
-        except ValueError:
-            self.fail('wrong_type', context='quantity', type='float')
-
-        return {
-            'stock_id': data['stock_id'],
-            'quantity': float(data['quantity'])
-        }
-
-class StockListField(serializers.ListField):
-    child = AddStockField()
 
 
 ### STOCK #####################################################################
