3151716688ed49614d7619f0de6bdee733b325a1
lhen4176 <lhen4176@uni.sydney.edu.au>
2022-11-08 22:00:54 +1100

    added core.user.models to test output

--- farmware/farmware/settings.py
+++ farmware/farmware/settings.py
@@ -207,6 +207,6 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=core.api.models',
+    '--cover-package=core.api.models,core.user.models',
     '--cover-html',
 ]
