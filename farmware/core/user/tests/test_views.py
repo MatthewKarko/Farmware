6f3807a01ebf7adab18fb3826d2f26d55674c8ce
Keegan <kpot0443@uni.sydney.edu.au>
2022-11-10 21:30:18 +1100

    added tests

--- farmware/core/user/tests/test_views.py
+++ farmware/core/user/tests/test_views.py
@@ -1,17 +1,18 @@
 from django.http import HttpResponse
-from django.test import Client, TestCase
 
-from rest_framework import serializers, exceptions
-from rest_framework import status
-from rest_framework.test import APITestCase, RequestsClient
+from rest_framework.test import APITestCase
 
 import json
 
 from ..models import User
-from ..serialisers import UserSerialiser
 from ...api.models.organisation import Organisation
 
-TEST_EMAIL = 'test@example.com'
+TEST_USER_EMAIL = 'user@example.com'
+TEST_ADMIN_EMAIL = 'admin@example.com'
+STRONG_PASSWORD = 'qWejf-djjd3-$dksm'
+FIELD_REQUIRED_RESPONSE = ["This field is required."]
+
+CONTENT_TYPE = "application/json"
 
 class UserViewSetTestCases(APITestCase):
     def setUp(self):
@@ -33,123 +34,162 @@ class UserViewSetTestCases(APITestCase):
             self.organisation.code, 
             self.password)
 
-    def test_register_new_user(self):
+    def test_successful_register_new_user(self):
+        """Test a successful user register."""
         payload = {
-                 'email' : TEST_EMAIL,
+                 'email' : TEST_USER_EMAIL,
             'first_name' : 'first_name',
              'last_name' : 'last_name',
-              'password' : 'password',
+              'password' : STRONG_PASSWORD,
               'org_code' : self.organisation.code
             }
+
         response: HttpResponse = self.client.post(
             '/api/user/register/user/', 
             json.dumps(payload), 
-            content_type="application/json"
+            content_type=CONTENT_TYPE
             )
-
-        print(response.content)
-        print(response.status_code)
 
         self.assertEquals(response.status_code, 201)
 
-    # def test_RegisteringUserError(self):
-    #     org= Organisation.objects.get(name="nameoforg")
-    #     response=self.client.post('/api/user/ /register/user/',{'first_name':'firstn','last_name':'lastn','password':'passwd','organisation':org})
-    #     self.assertEquals(response.status_code,400)
+        user: User = User.objects.get(**{'email': TEST_USER_EMAIL})
+        # TODO: change this when TAG(EMAIL_CONFIRMATION) is activated
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff, "User should not be a staff.")
+        self.assertFalse(user.is_superuser, "User should not be a superuser.")
+        self.assertEqual(user.role, User.Roles.WORKER.value)
 
-    # def test_RegisteringAdmin(self):
-    #     user = User.objects.first()
-    #     self.client.force_authenticate(user)
-    #     org= Organisation.objects.get(name="nameoforg")
-    #     response=self.client.post('/api/user/ /register/admin/',{'email': 'example@gmail.com','first_name':'firstn','last_name':'lastn','password':'passwd','org_name':org.pk})
-    #     self.assertEquals(response.status_code,201)
-
-    # def test_RegisteringAdminError(self):
-    #     serializer_data={}
-    #     response=self.client.post('/api/user/ /register/admin/',serializer_data)
-    #     self.assertEquals(response.status_code,status.HTTP_400_BAD_REQUEST)
-
-    # def test_estingSettingPasswordsError(self):
-    #     user = User.objects.first()
-    #     self.client.force_authenticate(user)
-    #     org= Organisation.objects.get(name="nameoforg")
-    #     serializer_data = {'email': 'example@gmail.com','first_name':'firstn','last_name':'lastn','password':'passwd','organisation':org}
-    #     response=self.client.post('/api/user/ /set_password/',serializer_data)
-    #     self.assertEquals(response.status_code,405)
-
-    # def test_testingSettingPasswords(self):
-    #     org= Organisation.objects.get(name="nameoforg")
-    #     serializer_data = {'email': 'example@gmail.com','first_name':'firstn','last_name':'lastn','password':'passwd','org_code':org.code}
-    #     response=self.client.post('/api/user/ /register/user/',serializer_data)
-    #     self.assertEquals(response.status_code,201)
-    #     user = User.objects.first()
-    #     self.client.force_authenticate(user)
-    #     serializer_data = {'email': 'example@gmail.com','first_name':'firstn','last_name':'lastn','old_password':'passwd','new_password':'pass','org_code':org.code}
-    #     respons=self.client.get('/api/user/ /set_password/',serializer_data,args={'pk':user.pk})
-    #     print(respons.content)
-    #     self.assertEquals(respons.status_code,200)
-
-    # def test_testingDestroy(self):
-    #     org= Organisation.objects.get(name="nameoforg")
-    #     #name =reverse('user-register-admin')
-    #     #raise ValueError(name)
-    #     response=self.client.post('/api/user/ /register/user/',{'email' : 'example@gmail.com','first_name':'firstn','last_name':'lastn','password':'passwd','org_name':org.name})
-    #     self.assertEquals(response.status_code,201)
-    #     response2=self.client.delete('/api/user/ /',serializer_data)
-    #     #user=response2[]
-    #     #urlwithid=' /'+user.pk
-    #     #response=c.delete(urlwithid,serializer_data)
-    #     #self.assertEquals(response.status_code,status.HTTP_200_OK)
-
-    # def test_testingList(self):
-    #     c = Client()
-    #     org= Organisation.objects.get(name="nameoforg")
-    #     serializer_data = {'email': 'example@gmail.com','first_name':'firstn','last_name':'lastn','password':'passwd','organisation':org}
-    #     response=c.post('/api/user/ /register/user/',serializer_data)
-    #     self.assertEquals(response.status_code,status.HTTP_201_CREATED)
-    #     response2=c.get('/api/user/ /',serializer_data)
-    #     self.assertNotEquals(response2,Null)
-
-    # def test_testingList(self):
-    #     org= Organisation.objects.get(name="nameoforg")
-    #     serializer_data = {'email': 'example@gmail.com','first_name':'firstn','last_name':'lastn','password':'passwd','org_name':org.name,'role':000}
-    #     respons=self.client.post('/api/user/ /register/admin/',serializer_data)
-    #     self.assertEquals(respons.status_code,status.HTTP_201_CREATED)
-    #     user = User.objects.first()
-    #     self.client.force_authenticate(user)
-    #     response = self.client.get('/api/user/ /')
-    #     self.assertEquals(response.status_code,200)
-
-    # def test_testingMyteams(self):
-    #     user = User.objects.first()
-    #     self.client.force_authenticate(user)
-
-    #     org= Organisation.objects.get(name="nameoforg")
-    #     team=Team.objects.create(category="sports",name="fifa team 12",organisation=org)
-    #     response=self.client.post('/api/user/ /register/user/',{'email': 'example@gmail.com','first_name':'firstn','last_name':'lastn','password':'passwd','org_code':org.code})
-    #     self.assertEquals(response.status_code,201)
-    #     respones=self.client.get('/api/user/ /teams/',{'user':user})
-    #     self.assertEquals(respones.status_code,200)
-
-    # def test_testingUserteams(self):
-    #     user = User.objects.first()
-    #     self.client.force_authenticate(user)
-    #     org=Organisation.objects.get(name="nameoforg")
-    #     team=Team.objects.create(category="sports",name="fifa team 12",organisation=org)
-    #     serializer_data = {'email': 'example@gmail.com','first_name':'firstn','last_name':'lastn','password':'passwd','org_name':org.name,'team':team}
-    #     response=self.client.post('/api/user/ /register/admin/',serializer_data)
-    #     self.assertEquals(response.status_code,201)
-    #     respons=self.client.get('/api/user/ /teams/',{'user':user})
-    #     #print(respons.content)
-    #     self.assertEquals(respons.status_code,200)
+    def test_missing_fields_register_new_user(self):
+        """Test an unsuccessful user register."""
+        fields = ['first_name', 'last_name', 'org_code']
         
-    # def testingPartialUpdate(self):
-    #     user = User.objects.first()
-    #     self.client.force_authenticate(user)
-    #     org= Organisation.objects.get(name="nameoforg")
-    #     response=self.client.post('/api/user/ /register/user/',{'email': 'example@gmail.com','first_name':'firstn','last_name':'lastn','password':'passwd','org_code':org.code})
-    #     #print(response.content)
-    #     self.assertEquals(response.status_code,201)
-    #     respons=self.client.patch(f'/api/user/ /{user.pk}/partial_update/',{'first_name':'firstn'})
-    #     #print(respons.content)
-    #     self.assertEquals(respons.status_code,2003)
+        for field in fields:
+            self.run_missing_field_register_new_user(field)
+
+    def run_missing_field_register_new_user(self, field: str):
+        """Run the test for a missing field when registering a new user."""
+        payload = {
+                 'email' : TEST_USER_EMAIL,
+            'first_name' : 'first_name',
+             'last_name' : 'last_name',
+              'password' : STRONG_PASSWORD,
+              'org_code' : self.organisation.code
+            }
+
+        payload.pop(field)
+
+        response: HttpResponse = self.client.post(
+            '/api/user/register/user/', 
+            json.dumps(payload), 
+            content_type=CONTENT_TYPE
+            )
+
+        content = json.loads(response.content)
+
+        self.assertEquals(response.status_code, 400)
+        self.assertTrue(content.get(field, False) == FIELD_REQUIRED_RESPONSE)
+
+    def test_weak_password_register_user(self):
+        """
+        Test creating a new user with weak passwords, i.e., too short, common,
+        etc.
+        """
+        # TODO
+        pass
+
+    def test_register_user_org_code_does_not_exist(self):
+        """Test creating a new user with a bad organisational code."""
+        # TODO
+        pass
+
+    def test_successful_register_admin(self):
+        """Test a successful admin register."""
+        payload = {
+                 'email' : TEST_ADMIN_EMAIL,
+            'first_name' : 'first_name',
+             'last_name' : 'last_name',
+              'password' : STRONG_PASSWORD,
+              'org_name' : 'Test Organisation'
+            }
+
+        response: HttpResponse = self.client.post(
+            '/api/user/register/admin/', 
+            json.dumps(payload), 
+            content_type=CONTENT_TYPE
+            )
+
+        self.assertEquals(response.status_code, 201)
+
+        user: User = User.objects.get(**{'email': TEST_ADMIN_EMAIL})
+        # TODO: change this when TAG(EMAIL_CONFIRMATION) is activated
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff, "Admin should not be a staff.")
+        self.assertFalse(user.is_superuser, "Admin should not be a superuser.")
+        self.assertEqual(user.role, User.Roles.ORGANISATION_ADMIN.value)        
+
+    def test_weak_password_register_admin(self):
+        """
+        Test creating a new admin with weak passwords, i.e., too short, common,
+        etc.
+        """
+        # TODO
+        pass
+
+    def test_register_admin_organisation_name_exists(self):
+        """
+        Test creating a new admin with a name that already exists. It should be
+        okay.
+        """
+        # TODO
+        pass
+
+    def test_set_password_successful(self):
+        """Test setting the password of the user to a valid password."""
+        # TODO
+        pass
+
+    def test_set_password_unsuccessful(self):
+        """Test setting the password of the user to something not valid."""
+        # TODO
+        pass
+
+    def test_removing_user(self):
+        """
+        Test destroying a user.
+
+        You should not be able destroy other users outside your organisation 
+        and if you are not an admin.
+        """
+        # TODO
+        pass
+
+    def test_list_users(self):
+        """
+        Test listing users.
+
+        This should only list users within their organisation.
+        Extra information should be displayed.
+        """
+        # TODO
+        pass
+
+    def test_getting_teams(self):
+        """
+        Test getting a user's teams.
+
+        Should only return their teams. 
+        """
+        # TODO
+        pass
+
+    def test_partial_updates(self):
+        """
+        Test partially updating user information.
+
+        - A user TODO(verify this claim) should not be able to change their 
+        password.
+        - A user should not be able to change their role.
+        - Anuser should not be able to change their organisation.
+        """
+        # TODO
+        pass
