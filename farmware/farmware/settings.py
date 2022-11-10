5a43694e547c97852ff81a508e1fce460e380d92
lhen4176 <lhen4176@uni.sydney.edu.au>
2022-11-10 14:11:15 +1100

    fixed test filtering

--- farmware/farmware/settings.py
+++ farmware/farmware/settings.py
@@ -207,6 +207,6 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=core.api,core.user',
+    '--cover-package=core.api.models,core.api.viewsets,core.user.viewsets,core.user.models',
     '--cover-html',
 ]
