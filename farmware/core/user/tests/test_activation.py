92be234b0abaee0c8656e49f3f592f242f9f2ce5
Keegan <kpot0443@uni.sydney.edu.au>
2022-11-11 08:55:03 +1100

    fixes

--- farmware/core/user/tests/test_activation.py
+++ farmware/core/user/tests/test_activation.py
@@ -1,14 +1,6 @@
-# TODO
-
-from django.db import IntegrityError, transaction
-from django.test import Client, TestCase
-
-from rest_framework import serializers, exceptions
-from rest_framework import status
-from rest_framework.test import APITestCase, RequestsClient
+from rest_framework.test import APITestCase
 
 from ..models import User
-from ..serialisers import UserSerialiser
 from ...api.models.organisation import Organisation
 
 
@@ -19,39 +11,4 @@ class ActivateAccountTestCases(APITestCase):
         self.organisation = Organisation.objects.create(
             code=org_code, name="Test Farm", logo="<logo>")
 
-    def test_testingGet(self):
-        #user = User.objects.first()
-        Org=Organisation.objects.get(name="nameoforg")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", Org,"password123")
-        self.client.force_authenticate(user)
-        tg=TokenGenerator()
-        request ={'user':user}
-        timestamp =time.time()
-        uidb64 = urlsafe_base64_encode(user.pk.to_bytes(5,'big'))
-        token=tg._make_hash_value(user, timestamp)
-        respons=self.client.post(f'/api/user/ /activate/{uidb64}/{token}/',{'user':user})
-        self.assertEquals(respons.status_code,200)
-
-    def test_testingGetError1(self):
-        c =Client()
-        Org=Organisation.objects.get(name="nameoforg")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", Org,"password123")
-        tg=TokenGenerator()
-        request ={'user':user}
-        timestamp =time.time()
-        uidb64 =None
-        token=tg._make_hash_value(user, timestamp)
-        response = c.get('activate/<uidb64>/<token>',request, uidb64, token)
-        self.assertEquals(response.status_code,404)
-        
-    def test_testingGetError2(self):
-        c =Client()
-        Org=Organisation.objects.get(name="nameoforg")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", Org,"password123")
-        tg=TokenGenerator()
-        request ={'user':user}
-        timestamp =time.time()
-        uidb64 = urlsafe_base64_encode(user.pk.to_bytes(5,'big'))
-        token=None
-        response = c.get('activate/<uidb64>/<token>',request, uidb64, token)
-        self.assertEquals(response.status_code,404)
+    # TODO
--- farmware/core/user/tests/test_tokens.py
+++ farmware/core/user/tests/test_tokens.py
@@ -1,5 +1,3 @@
-# TODO
-
 from django.db import IntegrityError, transaction
 from django.test import Client, TestCase
 
@@ -10,6 +8,9 @@ from rest_framework.test import APITestCase, RequestsClient
 from ..models import User
 from ..serialisers import UserSerialiser
 from ...api.models.organisation import Organisation
+from ..tokens import TokenGenerator
+
+import time
 
 
 class BlacklistTokenUpdateViewTestCases(TestCase):
@@ -19,23 +20,4 @@ class BlacklistTokenUpdateViewTestCases(TestCase):
         self.organisation = Organisation.objects.create(
             code=org_code, name="Test Farm", logo="<logo>")
 
-    def test_testingPostError(self):
-        Org=Organisation.objects.get(name="nameoforg")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", Org,"password123")
-        tg=TokenGenerator()
-        c=Client()
-        timestamp =time.time()
-        token=tg._make_hash_value(user, timestamp)
-        request={'refresh_token':token}
-        response = c.post('logout/blacklist/',request)
-        self.assertEquals(response.status_code,404)
-        
-    def test_posting(self):
-        Org=Organisation.objects.get(name="nameoforg")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", Org,"password123")
-        tg=TokenGenerator()
-        c=Client()
-        token = account_activation_token
-        request={'refresh_token':token}
-        response = c.post('logout/blacklist/',request)
-        self.assertEquals(response.status_code,404)
+    # TODO
