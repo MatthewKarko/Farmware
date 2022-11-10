d2257d8cecc5e0cf2d8863e93305b9dc208c14c3
sand5508 <sand5508@uni.sydney.edu.au>
2022-11-10 21:10:57 +1100

    added stock testcases

--- farmware/core/api/testStockViewset.py
+++ farmware/core/api/testStockViewset.py
@@ -1,165 +1,93 @@
-from django.test import TestCase
-from django.contrib.auth import get_user_model
-from .models.organisation import *
-from .models.team import *
-from .models.areacode import *
-from .models.produce import *
-from .models.stock import *
-from .models.supplier import *
-from .models.customer import *
-from .models.order import *
-from django.db import IntegrityError
-from django.core.exceptions import ValidationError
-from django_test_migrations.migrator import Migrator
-from django.db import migrations, models
-from core.api.migrations import *
-from .urls import * #0001_initial,0002_initial,0003_auto_20221018_0824,0004_auto_20221018_1055,0005_auto_20221018_1132
-from django_test_migrations.contrib.unittest_case import MigratorTestCase
-from django.test import Client
 from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from .models import *
 
 class StockViewsetTestCases(APITestCase):
+    # Create a Stock item
     def setUp(self):
-        org_code=generate_random_org_code()
-        Organisation.objects.create(code =org_code,name="Farmone",logo="goat")
-        organisatio=Organisation.objects.get(name="Farmone")
-        produce=Produce.objects.create(organisation=organisatio,name="eggs")
-        producequantitysuffix=ProduceQuantitySuffix.objects.create(produce_id=produce,suffix="lorem ipsum",base_equivalent=5.0)
-        producevariety=ProduceVariety.objects.create(produce_id=produce,variety="brown")
-        supplier=Supplier.objects.create(organisation=organisatio,name = "john",phone_number = "1234567891")
-        areacode=AreaCode.objects.create( organisation=organisatio,area_code="204",description="just another area code")
-        stock=Stock.objects.create(organisation=organisatio,produce_id =produce,
-    variety_id =producevariety,
-    quantity = 6.0,
-    quantity_suffix_id =producequantitysuffix,
-    supplier_id =supplier,
-    date_seeded = "2022-10-25",
-    date_planted = "2022-10-26",
-    date_picked = "2022-10-27",
-    ehd = "2022-10-28" ,
-    date_completed ="2022-10-29",
-    area_code_id=areacode)
-    def test_creating(self):
-        organisatio=Organisation.objects.get(name="Farmone")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", organisatio.code, None,is_staff=True)
-        self.client.force_authenticate(user)
-        areacode=AreaCode.objects.get(area_code="204")
-        produce=Produce.objects.get(name="eggs")
-        producevariety=ProduceVariety.objects.get(variety="brown")
-        producequantitysuffix=ProduceQuantitySuffix.objects.get(suffix="lorem ipsum")
-        supplier=Supplier.objects.get(name = "john")
-        response=self.client.post('/api/stock/',{'organisation':organisatio,'produce_id' :produce.pk,
-    'variety_id' :producevariety.pk,
-    'quantity' :6.0,
-    'quantity_suffix_id' :producequantitysuffix.pk,
-    'supplier_id' :supplier.pk,
-    'date_seeded' : "2022-10-25",
-    'date_planted' : "2022-10-26",
-    'date_picked' : "2022-10-27",
-    'ehd': "2022-10-28" ,
-    'date_completed' :"2022-10-29",
-    'area_code_id':areacode.pk})
-        self.assertEquals(response.status_code,200)
-    def test_destroying(self):
-        organisatio=Organisation.objects.get(name="Farmone")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", organisatio.code, None,is_staff=True)
-        self.client.force_authenticate(user)
-        areacode=AreaCode.objects.get(area_code="204")
-        produce=Produce.objects.get(name="eggs")
-        producevariety=ProduceVariety.objects.get(variety="brown")
-        producequantitysuffix=ProduceQuantitySuffix.objects.get(suffix="lorem ipsum")
-        supplier=Supplier.objects.get(name = "john")
-        response=self.client.post('/api/stock/',{'organisation':organisatio,'produce_id' :produce.pk,
-    'variety_id' :producevariety.pk,
-    'quantity' :6.0,
-    'quantity_suffix_id' :producequantitysuffix.pk,
-    'supplier_id' :supplier.pk,
-    'date_seeded' : "2022-10-25",
-    'date_planted' : "2022-10-26",
-    'date_picked' : "2022-10-27",
-    'ehd': "2022-10-28" ,
-    'date_completed' :"2022-10-29",
-    'area_code_id':areacode.pk})
-        self.assertEquals(response.status_code,200)
-        response=self.client.delete(f'/api/stock/{user.pk}/')
-        self.assertEquals(response.status_code,200)
-    def test_partial_update(self):
-        organisatio=Organisation.objects.get(name="Farmone")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", organisatio.code, None,is_staff=True)
-        self.client.force_authenticate(user)
-        areacode=AreaCode.objects.get(area_code="204")
-        produce=Produce.objects.get(name="eggs")
-        producevariety=ProduceVariety.objects.get(variety="brown")
-        producequantitysuffix=ProduceQuantitySuffix.objects.get(suffix="lorem ipsum")
-        supplier=Supplier.objects.get(name = "john")
-        response=self.client.post('/api/stock/',{'organisation':organisatio,'produce_id' :produce.pk,
-    'variety_id' :producevariety.pk,
-    'quantity' :6.0,
-    'quantity_suffix_id' :producequantitysuffix.pk,
-    'supplier_id' :supplier.pk,
-    'date_seeded' : "2022-10-25",
-    'date_planted' : "2022-10-26",
-    'date_picked' : "2022-10-27",
-    'ehd': "2022-10-28" ,
-    'date_completed' :"2022-10-29",
-    'area_code_id':areacode.pk})
-        self.assertEquals(response.status_code,200)
-        response=self.client.patch(f'/api/stock/{user.pk}/',{'quantity' :7.0})
-        self.assertEquals(response.status_code,200)
-    def test_update(self):
-        organisatio=Organisation.objects.get(name="Farmone")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", organisatio.code, None,is_staff=True)
-        self.client.force_authenticate(user)
-        areacode=AreaCode.objects.get(area_code="204")
-        produce=Produce.objects.get(name="eggs")
-        producevariety=ProduceVariety.objects.get(variety="brown")
-        producequantitysuffix=ProduceQuantitySuffix.objects.get(suffix="lorem ipsum")
-        supplier=Supplier.objects.get(name = "john")
-        response=self.client.post('/api/stock/',{'organisation':organisatio,'produce_id' :produce.pk,
-    'variety_id' :producevariety.pk,
-    'quantity' :6.0,
-    'quantity_suffix_id' :producequantitysuffix.pk,
-    'supplier_id' :supplier.pk,
-    'date_seeded' : "2022-10-25",
-    'date_planted' : "2022-10-26",
-    'date_picked' : "2022-10-27",
-    'ehd': "2022-10-28" ,
-    'date_completed' :"2022-10-29",
-    'area_code_id':areacode.pk})
-        self.assertEquals(response.status_code,200)
-        response=self.client.put(f'/api/stock/{user.pk}/',{'organisation':organisatio,'produce_id' :produce.pk,
-    'variety_id' :producevariety.pk,
-    'quantity' :6.0,
-    'quantity_suffix_id' :producequantitysuffix.pk,
-    'supplier_id' :supplier.pk,
-    'date_seeded' : "2022-10-25",
-    'date_planted' : "2022-10-26",
-    'date_picked' : "2022-10-27",
-    'ehd': "2022-10-28" ,
-    'date_completed' :"2022-10-29",
-    'area_code_id':areacode.pk})
-        self.assertEquals(response.status_code,200)
+        self.org_code = generate_random_org_code()
+        self.org = Organisation.objects.create(code = self.org_code, name = "Org", logo="img")
+        self.p_name = "Eggs"
+        self.p = Produce.objects.create(organisation=self.org, name=self.p_name)
+        self.pv_name = "Variety"
+        self.pv = ProduceVariety.objects.create(variety=self.pv_name, produce_id=self.p)
+        self.pqs_suffix = "kg"
+        self.pqs_base = 1000
+        self.pqs = ProduceQuantitySuffix.objects.create(suffix=self.pqs_suffix, base_equivalent = self.pqs_base, produce_id = self.p)
+        self.s_name = "Supplier"
+        self.s_pn = "012456789"
+        self.s = Supplier.objects.create(organisation=self.org, name = self.s_name, phone_number = self.s_pn)
+        self.ac_name = "Area Code"
+        self.ac_desc = "It's an area code"
+        self.ac = AreaCode.objects.create(organisation=self.org, area_code = self.ac_name, description = self.ac_desc)
+        self.qty = 123.0
+        self.aqty = 123.0
+        self.date_seeded = "2022-10-01"
+        self.date_planted = "2022-10-02"
+        self.date_picked = "2022-10-03"
+        self.ehd = "2022-12-01"
+        self.stock = Stock.objects.create(organisation=self.org, quantity=self.qty, quantity_available=self.aqty, date_seeded=self.date_seeded, date_planted = self.date_planted, date_picked = self.date_picked, ehd = self.ehd, date_completed = None, produce_id = self.p, variety_id = self.pv, quantity_suffix_id = self.pqs, supplier_id = self.s, area_code_id = self.ac)
+        self.template_list = [{"id":1, "quantity":self.qty, "quantity_available":self.aqty, "date_seeded":self.date_seeded, "date_planted":self.date_planted, "date_picked":self.date_picked, "ehd":self.ehd, "date_completed": None, "organisation":self.org_code, "produce_id": 1, "variety_id": 1, "quantity_suffix_id": 1, "supplier_id": 1, "area_code_id": 1, "produce_name": self.p_name, "variety_name": self.pv_name, "quantity_suffix_name": self.pqs_suffix, "base_equivalent": self.pqs_base, "area_code_name": self.ac_name, "area_code_description": self.ac_desc, "supplier_name": self.s_name, "supplier_phone_number": self.s_pn}]
+        self.template = {"id":1, "quantity":self.qty, "quantity_available":self.aqty, "date_seeded":self.date_seeded, "date_planted":self.date_planted, "date_picked":self.date_picked, "ehd":self.ehd, "date_completed": None, "organisation":self.org_code, "produce_id": 1, "variety_id": 1, "quantity_suffix_id": 1, "supplier_id": 1, "area_code_id": 1}
+        self.user = get_user_model().objects.create_user("test", "test", "test", self.org_code, None, is_staff=True)
+        self.client.force_authenticate(self.user)
+
+    # Test output of GET list
     def test_list(self):
-        organisatio=Organisation.objects.get(name="Farmone")
-        user=get_user_model().objects.create_user("email@gmail.com", "first_name", "last_name", organisatio.code, None,is_staff=True)
-        self.client.force_authenticate(user)
-        areacode=AreaCode.objects.get(area_code="204")
-        produce=Produce.objects.get(name="eggs")
-        producevariety=ProduceVariety.objects.get(variety="brown")
-        producequantitysuffix=ProduceQuantitySuffix.objects.get(suffix="lorem ipsum")
-        supplier=Supplier.objects.get(name = "john")
-        response=self.client.post('/api/stock/',{'organisation':organisatio,'produce_id' :produce.pk,
-    'variety_id' :producevariety.pk,
-    'quantity' :6.0,
-    'quantity_suffix_id' :producequantitysuffix.pk,
-    'supplier_id' :supplier.pk,
-    'date_seeded' : "2022-10-25",
-    'date_planted' : "2022-10-26",
-    'date_picked' : "2022-10-27",
-    'ehd': "2022-10-28" ,
-    'date_completed' :"2022-10-29",
-    'area_code_id':areacode.pk})
-        self.assertEquals(response.status_code,200)
-        response=self.client.get('/api/stock/')
-        res=response.json()
-        self.assertEquals(res[0]['quantity'],6.0)
+        self.assertJSONEqual(str(self.client.get('/api/stock/').content, encoding='utf-8'), self.template_list)
+    
+    # Test output of GET read
+    def test_read(self):
+        self.assertJSONEqual(str(self.client.get('/api/stock/1/').content, encoding='utf-8'), self.template)
+
+    # Test PATCH functionality
+    def test_partial_update(self):
+        output = self.template
+        output["quantity"] = 100.12
+        self.assertEquals(self.client.patch('/api/stock/1/', {"quantity":100.12}).status_code, 200)
+        self.assertJSONEqual(str(self.client.get('/api/stock/1/').content, encoding='utf-8'), output)
+        output["quantity_available"] = 99.99
+        self.assertEquals(self.client.patch('/api/stock/1/', {"quantity_available":99.99}).status_code, 200)
+        self.assertJSONEqual(str(self.client.get('/api/stock/1/').content, encoding='utf-8'), output)
+        output["date_seeded"] = "2022-09-15"
+        self.assertEquals(self.client.patch('/api/stock/1/', {"date_seeded":"2022-09-15"}).status_code, 200)
+        self.assertJSONEqual(str(self.client.get('/api/stock/1/').content, encoding='utf-8'), output)
+        output["date_planted"] = "2022-09-16"
+        self.assertEquals(self.client.patch('/api/stock/1/', {"date_planted":"2022-09-16"}).status_code, 200)
+        self.assertJSONEqual(str(self.client.get('/api/stock/1/').content, encoding='utf-8'), output)
+        output["date_picked"] = "2022-09-17"
+        self.assertEquals(self.client.patch('/api/stock/1/', {"date_picked":"2022-09-17"}).status_code, 200)
+        self.assertJSONEqual(str(self.client.get('/api/stock/1/').content, encoding='utf-8'), output)
+        output["ehd"] = "2022-09-18"
+        self.assertEquals(self.client.patch('/api/stock/1/', {"ehd":"2022-09-18"}).status_code, 200)
+        self.assertJSONEqual(str(self.client.get('/api/stock/1/').content, encoding='utf-8'), output)
+        output["date_completed"] = "2022-12-19"
+        self.assertEquals(self.client.patch('/api/stock/1/', {"date_completed":"2022-12-19"}).status_code, 200)
+        self.assertJSONEqual(str(self.client.get('/api/stock/1/').content, encoding='utf-8'), output)
+
+    # Test PUT functionality
+    def test_update(self):
+        output = self.template
+        output["quantity"] = 100.12
+        output["quantity_available"] = 99.99
+        output["date_seeded"] = "2022-09-15"
+        output["date_planted"] = "2022-09-16"
+        output["date_picked"] = "2022-09-17"
+        output["ehd"] = "2022-09-18"
+        output["date_completed"] = "2022-12-19"
+        self.assertEquals(self.client.put('/api/stock/1/', output).status_code, 200)
+        self.assertJSONEqual(str(self.client.get('/api/stock/1/').content, encoding='utf-8'), output)
+    
+    # Test DELETE functionality
+    def test_delete(self):
+        self.assertEquals(len(Stock.objects.all()), 1)
+        self.assertEquals(self.client.delete('/api/stock/1/').status_code, 200)
+        self.assertEquals(len(Stock.objects.all()), 0)
+
+    # Test toggle date completed API endpoint
+    def test_toggle_date_completed(self):
+        self.assertEquals(self.client.get('/api/stock/1/toggle_date_completed/').status_code, 200)
+        self.assertIsNotNone(self.client.get('/api/stock/1/').json()["date_completed"])
+        self.assertEquals(self.client.get('/api/stock/1/toggle_date_completed/').status_code, 200)
+        self.assertIsNone(self.client.get('/api/stock/1/').json()["date_completed"])
+        
--- farmware/core/api/testStockmodel.py
+++ farmware/core/api/testStockmodel.py
@@ -9,38 +9,49 @@ from .models.supplier import *
 from .models.customer import *
 from .models.order import *
 from django.db import IntegrityError
-from django.core.exceptions import ValidationError
-from django_test_migrations.migrator import Migrator
-from django.db import migrations, models
-from core.api.migrations import *
-from .urls import * #0001_initial,0002_initial,0003_auto_20221018_0824,0004_auto_20221018_1055,0005_auto_20221018_1132
-from django_test_migrations.contrib.unittest_case import MigratorTestCase
-from django.test import Client
-from rest_framework.test import APITestCase
-
 
 class StockTestCases(TestCase):
+    # Create a Stock item
     def setUp(self):
-        org_code=generate_random_org_code()
-        Organisation.objects.create(code =org_code,name="Farmon",logo="oat")
-        organisatio=Organisation.objects.get(name="Farmon")
-        produce=Produce.objects.create(organisation=organisatio,name="eggs")
-        producequantitysuffix=ProduceQuantitySuffix.objects.create(produce_id=produce,suffix="lorem ipsum",base_equivalent=5.0)
-        producevariety=ProduceVariety.objects.create(produce_id=produce,variety="brown")
-        supplier=Supplier.objects.create(organisation=organisatio,name = "john",phone_number = "1234567891")
-        areacode=AreaCode.objects.create( organisation=organisatio,area_code="204",description="just another area code")
-        Stock.objects.create(organisation=organisatio,produce_id =produce,
-    variety_id =producevariety,
-    quantity = 6.0,
-    quantity_suffix_id =producequantitysuffix,
-    supplier_id =supplier,
-    date_seeded = "2022-10-25",
-    date_planted = "2022-10-26",
-    date_picked = "2022-10-27",
-    ehd = "2022-10-28" ,
-    date_completed ="2022-10-29",
-    area_code_id =areacode )
-    def test_Stock1(self):
-        stock=Stock.objects.get(quantity=6.0)
-        #d=new Date("2022-10-28")
-        #self.assertEqual(stock.ehd,d)
+        self.org_code = generate_random_org_code()
+        self.org = Organisation.objects.create(code = self.org_code, name = "Org", logo="img")
+        self.p_name = "Eggs"
+        self.p = Produce.objects.create(organisation=self.org, name=self.p_name)
+        self.pv_name = "Variety"
+        self.pv = ProduceVariety.objects.create(variety=self.pv_name, produce_id=self.p)
+        self.pqs_suffix = "kg"
+        self.pqs_base = 1000
+        self.pqs = ProduceQuantitySuffix.objects.create(suffix=self.pqs_suffix, base_equivalent = self.pqs_base, produce_id = self.p)
+        self.s_name = "Supplier"
+        self.s_pn = "012456789"
+        self.s = Supplier.objects.create(organisation=self.org, name = self.s_name, phone_number = self.s_pn)
+        self.ac_name = "Area Code"
+        self.ac_desc = "It's an area code"
+        self.ac = AreaCode.objects.create(organisation=self.org, area_code = self.ac_name, description = self.ac_desc)
+        self.qty = 123
+        self.aqty = 123
+        self.date_seeded = "2022-10-01"
+        self.date_planted = "2022-10-02"
+        self.date_picked = "2022-10-03"
+        self.ehd = "2022-12-01"
+        self.stock = Stock.objects.create(organisation=self.org, quantity=self.qty, quantity_available=self.aqty, date_seeded=self.date_seeded, date_planted = self.date_planted, date_picked = self.date_picked, ehd = self.ehd, date_completed = None, produce_id = self.p, variety_id = self.pv, quantity_suffix_id = self.pqs, supplier_id = self.s, area_code_id = self.ac)
+
+    # Test whether the fields of the created stock are correct or not
+    def test_stock_fields(self):
+        self.assertEquals(self.stock.quantity, self.qty)
+        self.assertEquals(self.stock.quantity_available, self.aqty)
+        self.assertEquals(self.stock.date_seeded, self.date_seeded)
+        self.assertEquals(self.stock.date_planted, self.date_planted)
+        self.assertEquals(self.stock.date_picked, self.date_picked)
+        self.assertEquals(self.stock.ehd, self.ehd)
+        self.assertEquals(self.stock.date_completed, None)
+        self.assertEquals(self.stock.produce_id, self.p)
+        self.assertEquals(self.stock.variety_id, self.pv)
+        self.assertEquals(self.stock.quantity_suffix_id, self.pqs)
+        self.assertEquals(self.stock.supplier_id, self.s)
+        self.assertEquals(self.stock.area_code_id, self.ac)
+
+    # Ensure that null org raises integrity error
+    def test_stock_creation_exception(self):
+        with self.assertRaises(IntegrityError):
+            Stock.objects.create(organisation=None, quantity_available=self.aqty, date_seeded=self.date_seeded, date_planted = self.date_planted, date_picked = self.date_picked, ehd = self.ehd, date_completed = None, produce_id = self.p, variety_id = self.pv, quantity_suffix_id = self.pqs, supplier_id = self.s, area_code_id = self.ac)
