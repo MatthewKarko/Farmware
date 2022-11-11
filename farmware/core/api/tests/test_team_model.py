3c70549cce185c144cbf75c55e3bb0a2a4c16ec3
lhen4176 <lhen4176@uni.sydney.edu.au>
2022-11-11 11:31:02 +1100

    removed validation from teams testing

--- farmware/core/api/tests/test_team_model.py
+++ farmware/core/api/tests/test_team_model.py
@@ -1,22 +1,34 @@
 from django.test import TestCase
 from ..models import *
-from django.core.exceptions import ValidationError
+# from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 
 class TeamTestCases(TestCase):
     def setUp(self):
         organisation=Organisation.objects.create(name="Farmone",logo="goat")
         Team.objects.create(category ="j",name = "jack",organisation = organisation)
 
-    def test_Team(self):
-        team=Team.objects.get(category ="j")
-        self.assertEqual(team.name,"jack")
-        organisation2=Organisation.objects.create(name="Farmon",logo="goa")
-        with self.assertRaises(ValidationError):
-            Team.objects.create(category ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi convallis euismod gravida. Vestibulum quam lacus, faucibus ac dui nec, hendrerit lobortis arcu. Ut vel lorem at enim dignissim porttitor in nec neque. Mauris lobortis justo lorem, id venenatis dui laoreet vel. Quisque egestas neque quis erat porttitor fermentum. Proin cursus, lorem non auctor aliquam, turpis neque tincidunt sem, vitae maximus massa dolor ut diam. Nam euismod urna sed leo vestibulum ultrices. Donec tempus fringilla feugiat. Nullam faucibus mattis diam, in sagittis lacus aliquam eget. Mauris eu ligula fermentum, bibendum enim id, sodales turpis. Praesent sed risus felis. Vivamus ut ultrices nisl.Ut luctus purus neque, eget blandit augue consectetur at. Etiam eleifend cursus tortor, ut venenatis lectus laoreet sollicitudin. Nunc at elementum magna. Integer ut scelerisque arcu, ac pellentesque lorem. Donec rutrum porttitor consectetur. Nunc nunc enim, sollicitudin mollis maximus non, lacinia at neque. Curabitur ultrices tincidunt pharetra. Vestibulum vitae.",name = "jack",organisation = organisation2)
-            raise ValidationError("error")
+    # Test Team fields are correct
+    def test_team_fields(self):
+        cat = Team.objects.get(category="j")
+        self.assertEqual(cat.category, "j")
+        self.assertEquals(cat.name,"jack")
+    
 
-    def test_team2(self):
-        organisation2=Organisation.objects.create(name="Farmon",logo="goa")
-        with self.assertRaises(ValidationError):
-            Team.objects.create(category ="Jack",name = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi convallis euismod gravida. Vestibulum quam lacus, faucibus ac dui nec, hendrerit lobortis arcu. Ut vel lorem at enim dignissim porttitor in nec neque. Mauris lobortis justo lorem, id venenatis dui laoreet vel. Quisque egestas neque quis erat porttitor fermentum. Proin cursus, lorem non auctor aliquam, turpis neque tincidunt sem, vitae maximus massa dolor ut diam. Nam euismod urna sed leo vestibulum ultrices. Donec tempus fringilla feugiat. Nullam faucibus mattis diam, in sagittis lacus aliquam eget. Mauris eu ligula fermentum, bibendum enim id, sodales turpis. Praesent sed risus felis. Vivamus ut ultrices nisl.Ut luctus purus neque, eget blandit augue consectetur at. Etiam eleifend cursus tortor, ut venenatis lectus laoreet sollicitudin. Nunc at elementum magna. Integer ut scelerisque arcu, ac pellentesque lorem. Donec rutrum porttitor consectetur. Nunc nunc enim, sollicitudin mollis maximus non, lacinia at neque. Curabitur ultrices tincidunt pharetra. Vestibulum vitae.",organisation = organisation2)
-            raise ValidationError("error")
+    # Test Team raises error if same name & category is used again.
+    def test_AreaCode_repeat_integrity_error(self):
+        org = Organisation.objects.get(name="Farmone")
+        with self.assertRaises(IntegrityError):
+            Team.objects.create(category ="j",name = "jack",organisation = org)
+
+    # Test that multiple Teams can be added to an organisation
+    def test_AreaCode_multiple(self):
+        org_code = generate_random_org_code()
+        Organisation.objects.create(code=org_code, name="Farm2", logo="go")
+        org = Organisation.objects.get(code=org_code)
+        Team.objects.create(category ="j123",name = "jack",organisation = org)
+        Team.objects.create(category ="b123",name = "bob",organisation = org)
+        self.assertEqual(Team.objects.get(
+            category="j123").name, "jack")
+        self.assertEqual(Team.objects.get(
+            category="b123").name, "bob")
