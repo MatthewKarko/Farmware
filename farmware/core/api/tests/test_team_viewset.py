166334089cb9bd85dab29927e31daf6895abbd3b
lhen4176 <lhen4176@uni.sydney.edu.au>
2022-11-11 11:47:49 +1100

    added team viewset field validation and unauthenticated test

--- farmware/core/api/tests/test_team_viewset.py
+++ farmware/core/api/tests/test_team_viewset.py
@@ -98,3 +98,46 @@ class TeamViewsetTestCases(APITestCase):
         self.assertEquals(res.status_code, 200)
         res = res.json()
         self.assertEquals(res, {'category': 'Oranges', 'name': 'OrangeCrazy'})
+
+    # Test that the team endpoints are not accessible to an unauthorised user.
+    def test_unauthorised_user(self):
+        # Test create is inaccessible
+        res=self.client.post('/api/team/',{'category':"Bananas",'name':"BananaCrazy"})
+        self.assertEquals(res.status_code,401)
+
+        # Test delete is inaccessible
+        res_del=self.client.delete(f'/api/team/0/')
+        self.assertEquals(res_del.status_code,401)
+
+        # Test update is inaccessible
+        res_update = self.client.put(f'/api/team/0/', {'category': 'Oranges'})
+        self.assertEquals(res_update.status_code, 401)
+
+        # Test patch in inaccessible
+        res_patch = self.client.patch(f'/api/team/0/', {})
+        self.assertEquals(res_patch.status_code, 401)
+
+        # Test list in inaccessible
+        res_get = self.client.get('/api/team/')
+        self.assertEquals(res_get.status_code, 401)
+
+
+    #Tests the field limits in Team model
+    def test_field_validation(self):
+        user=get_user_model().objects.create_superuser(email="email@gmail.com",first_name= "first_name",last_name= "last_name",password=None)
+        self.client.force_authenticate(user)
+
+        #first check area code beneath limits
+        response = self.client.post(
+            '/api/team/', {'category': "a"*100, 'name': "d"*100})
+        self.assertEquals(response.status_code, 200)
+
+        #now exceed name limit
+        response = self.client.post(
+            '/api/team/', {'category': "a"*100, 'name': "d"*101})
+        self.assertEquals(response.status_code, 400)
+
+        #now exceed category limit
+        response = self.client.post(
+            '/api/team/', {'category': "a"*101, 'name': "d"*100})
+        self.assertEquals(response.status_code, 400)
